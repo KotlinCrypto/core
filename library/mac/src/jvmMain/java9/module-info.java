49d0988d4c2dbcc933ef23e89216da11ce63cb68
Matthew Nelson <developer@matthewnelson.io>
2024-03-18 17:46:09 -0400

    Add `transitive` keyword for module dependencies (#58)

--- library/mac/src/jvmMain/java9/module-info.java
+++ library/mac/src/jvmMain/java9/module-info.java
@@ -1,6 +1,6 @@
 module org.kotlincrypto.core.mac {
     requires kotlin.stdlib;
-    requires org.kotlincrypto.core;
+    requires transitive org.kotlincrypto.core;
 
     exports org.kotlincrypto.core.mac;
 }
