b90e1ab7ce461bd32ae18ff6236fb62127898ebb
Matthew Nelson <developer@matthewnelson.io>
2024-03-18 12:41:49 -0400

    Add JPMS support via Multi-Release Jar

---
+++ library/digest/src/jvmMain/java9/module-info.java
@@ -0,0 +1,7 @@
+module org.kotlincrypto.core.digest {
+    requires kotlin.stdlib;
+    requires org.kotlincrypto.core;
+
+    exports org.kotlincrypto.core.digest;
+    exports org.kotlincrypto.core.digest.internal;
+}
---
+++ library/xof/src/jvmMain/java9/module-info.java
@@ -0,0 +1,7 @@
+module org.kotlincrypto.core.xof {
+    requires kotlin.stdlib;
+    requires org.kotlincrypto.core;
+    requires org.kotlincrypto.endians;
+
+    exports org.kotlincrypto.core.xof;
+}
