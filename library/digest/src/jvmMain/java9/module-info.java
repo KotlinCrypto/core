05b35b4f931ca1de1340363711e490d25e2ab48e
Matthew Nelson <developer@matthewnelson.io>
2024-03-18 17:24:40 -0400

    Add transitive keyword for module dependencies

--- library/digest/src/jvmMain/java9/module-info.java
+++ library/digest/src/jvmMain/java9/module-info.java
@@ -1,6 +1,6 @@
 module org.kotlincrypto.core.digest {
     requires kotlin.stdlib;
-    requires org.kotlincrypto.core;
+    requires transitive org.kotlincrypto.core;
 
     exports org.kotlincrypto.core.digest;
     exports org.kotlincrypto.core.digest.internal;
